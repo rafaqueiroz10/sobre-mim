ba05a910d8883c5ab51d5a29c6473aae1f53a5e5
Rafael Queiroz Vilaça <112505616+rafaqueiroz10@users.noreply.github.com>
2024-02-11 23:20:59 -0300

    Update and rename carregando.js to interface.js

---
+++ interface.js
@@ -0,0 +1,69 @@
+let carregandoEl = document.querySelector('.carregando'),
+    headerEl = document.querySelector('header'),
+    linkGitHubEl = document.querySelector('#link-github'),
+    linkInstagramEl = document.querySelector('#link-instagram'),
+    linkGmailEl = document.querySelector('#link-gmail'),
+    arrSectionEl = document.querySelectorAll('section'),
+    linksDoHeaderEl = document.querySelectorAll('header nav ul li'),
+    sobreMimEl = document.querySelector ('.sobre-mim');
+
+linkGitHubEl.title = 'GitHub';
+linkInstagramEl.title = 'Instagram';
+linkGmailEl.title = 'Gmail';
+
+function abrirPagina() {
+    carregandoEl.classList.add('ocultar');
+    headerEl.classList.remove('ocultar');
+    headerEl.classList.add ('animacao-revelar');
+    for (let sectionEl of arrSectionEl) {
+        sectionEl.classList.add ('animacao-revelar');
+    }
+    sobreMimEl.classList.remove ('ocultar');
+}
+
+setTimeout(abrirPagina, 3000);
+
+// vai abrir cada um dos links em uma nova aba
+linkGitHubEl.addEventListener('click', () => {
+    let url = 'https://github.com/rafaqueiroz10';
+    let linkEmNovaAba = open(url, '_blank');
+    linkEmNovaAba.focus();
+});
+
+linkGmailEl.addEventListener('click', () => {
+    let url = 'https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRlRZVbfvFrnpCZplkLlpvCWltrPLMtJbtQzbwqHHqZzkmttrWKRtZDCrmZgNChjBVhgtRb';
+    let linkEmNovaAba = open(url, '_blank');
+    linkEmNovaAba.focus();
+});
+
+linkInstagramEl.addEventListener('click', () => {
+    let url = 'https://www.instagram.com/rqueiroz17/';
+    let linkEmNovaAba = open(url, '_blank');
+    linkEmNovaAba.focus();
+});
+
+function marcaLink (linkEl) {
+    for (let linkEl of linksDoHeaderEl) {
+        linkEl.classList.remove('link-selecionado');
+    }
+    linkEl.classList.add('link-selecionado');
+}
+
+function abrirSection(linkEl) {
+    for (let sectionEl of arrSectionEl) {
+        sectionEl.classList.add('ocultar');
+    }
+    let c = linkEl.innerHTML.toLowerCase();
+    c = c.replaceAll('?', '');
+    c = c.replaceAll(' ', '-');
+    let sectionDaPaginaEl = document.querySelector('.' + c);
+    sectionDaPaginaEl.classList.remove('ocultar');
+}
+
+for (let linkEl of linksDoHeaderEl) {
+    linkEl.addEventListener('click', (e) => {
+        let linkClicadoEl = e.currentTarget;
+        marcaLink (linkClicadoEl);
+        abrirSection (linkClicadoEl);
+    });
+}
